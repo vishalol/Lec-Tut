d22dd146cb8829d30832a298a2c549232af3b631
VishalCR7 <vs383885@outlook.com>
2017-04-03 00:37:45 +0530

    almost complete

--- lectut/views.py
+++ lectut/views.py
@@ -35,6 +35,7 @@ def coursepage(request,user_id, course_id):
 def profile(request, user_id, ):
 	if request.user.is_authenticated():
 		user = get_object_or_404(User, pk=user_id)
+		latest_post = Post.objects.order_by('-pub_date')[:5]
 		if(hasattr(user, 'student')):
 			student = user.student
 			return render(request, 'lectut/profile.html', {'user': student, 'userid': user_id})
