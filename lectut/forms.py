b9f987f243b7293dbf0479e260e02cc09b8ac8cd
VishalCR7 <vs383885@outlook.com>
2017-04-05 01:03:25 +0530

    added login

--- lectut/forms.py
+++ lectut/forms.py
@@ -1,5 +1,13 @@
+import re
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+
+TITLE_CHOICES = (
+    ('Student', 'Student'),
+    ('Proffessor', 'Proffessor'),
+)
 
 class LoginForm(forms.Form):
 	username = forms.CharField(label='Username', max_length=30)
@@ -9,4 +17,42 @@ class PostForm(forms.Form):
 	Post = forms.CharField( widget=forms.Textarea)
 
 class CommentForm(forms.Form):
-	Comment = forms.CharField(label='Comment', max_length=300)	
+	Comment = forms.CharField(label='Comment', max_length=300)
+
+class SignupForm(forms.Form):
+	username = forms.CharField(label='Username', max_length=30)
+	email = forms.EmailField(label='Email')
+	password1 = forms.CharField(label='Password',widget=forms.PasswordInput())
+	password2 = forms.CharField(label='Password (Again)',widget=forms.PasswordInput())
+	typ = forms.CharField(max_length=20,widget=forms.Select(choices=TITLE_CHOICES))
+
+	def clean_password2(self):
+		if 'password1' in self.cleaned_data:
+			password1 = self.cleaned_data['password1']
+			password2 = self.cleaned_data['password2']
+			if password1 == password2:
+				return password2
+		raise forms.ValidationError('Passwords do not match.')
+
+	def clean_username(self):
+		username = self.cleaned_data['username']
+		if not re.search(r'^\w+$', username):
+			raise forms.ValidationError('Username can only contain alphanumeric characters and the underscore.')
+		try:
+			User.objects.get(username=username)
+		except ObjectDoesNotExist:
+			return username
+		raise forms.ValidationError('Username is already taken.')
+
+
+class StudentSignup(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['name', 'branch', 'year', 'courses']
+        exclude = ['user']
+
+class ProffSignup(forms.ModelForm):
+    class Meta:
+        model = Proff
+        fields = ['name', 'courses']
+        exclude = ['user']        		
--- lectut/views.py
+++ lectut/views.py
@@ -13,28 +13,28 @@ def index(request):
 		user = request.user
 		if(hasattr(user, 'student')):
 			student = user.student
-			return render(request, 'lectut/profile.html', {'user': student, 'userid': user.id})
+			return HttpResponseRedirect(reverse('lectut:profile'))
 		else:
 			proff = user.proff
-			return render(request, 'lectut/profile.html', {'user':proff, 'userid': user.id})
+			return HttpResponseRedirect(reverse('lectut:profile'))
 	form = LoginForm()
 	return render(request, 'lectut/index.html', {'form': form})
 
-def coursepage(request,user_id, course_id):
+def coursepage(request, course_id):
 	if request.user.is_authenticated():
 		form1 = PostForm()
 		form2 = CommentForm()
 		course = get_object_or_404(Course, pk=course_id)
-		return render(request, 'lectut/course.html', {'course': course,  'userid': user_id, 'form1': form1, 'form2':form2})
+		return render(request, 'lectut/course.html', {'course': course,  'userid': request.user.id, 'form1': form1, 'form2':form2})
 	else:
 		messages.error(request, 'You have to login 1st')
 		return HttpResponseRedirect(reverse('lectut:index'))
 		
 
 
-def profile(request, user_id, ):
+def profile(request ):
 	if request.user.is_authenticated():
-		user = get_object_or_404(User, pk=user_id)
+		user = request.user
 		latest_post = Post.objects.order_by('-pub_time')[:5]
 		form2 = CommentForm()
 		
@@ -50,7 +50,7 @@ def profile(request, user_id, ):
 
 					list3.append(course)
 
-			return render(request, 'lectut/profile.html', {'user': student, 'latestpost':zip(list2, list3),'userid': user_id, 'form2':form2})
+			return render(request, 'lectut/profile.html', {'user': student, 'latestpost':zip(list2, list3),'userid': user.id, 'form2':form2})
 		else:
 			proff = user.proff
 			list1 = proff.courses.all()
@@ -62,7 +62,7 @@ def profile(request, user_id, ):
 					list2.append(pos[0])
 
 					list3.append(course)
-			return render(request, 'lectut/profile.html', {'user':proff, 'latestpost':zip(list2, list3),'userid': user_id, 'form2':form2})
+			return render(request, 'lectut/profile.html', {'user':proff, 'latestpost':zip(list2, list3),'userid': user.id, 'form2':form2})
 
 	else:
 		messages.error(request, 'You have to login 1st')
@@ -79,7 +79,7 @@ def log(request):
 			user = authenticate(username=username, password=password)
 			if user is not None:
 				login(request, user)
-				return HttpResponseRedirect(reverse('lectut:profile', args=(user.id,)))
+				return HttpResponseRedirect(reverse('lectut:profile'))
 			else :
 				form = LoginForm()
 				return render(request, 'lectut/index.html',{'form':form, 'error_message':"Incorrect Credentials"})
@@ -94,22 +94,62 @@ def logo(request):
 
 
 
-def posting(request,user_id, course_id):
+def posting(request, course_id):
 		if request.method == 'POST':
-			user = get_object_or_404(User, pk=user_id)
+			user = request.user
+			if(hasattr(user, 'student')):
+				student = user.student
+				course1 = student.courses.all()
+			else:
+				proff = user.proff
+				course1 = proff.courses.all()
 			course = get_object_or_404(Course, pk=course_id)
-			course.post_set.create(post=request.POST['Post'], poster=user.username)
-			return HttpResponseRedirect(reverse('lectut:coursepage', args=(user.id,course_id)))
+			if course in course1:
+				course.post_set.create(post=request.POST['Post'], poster=user.username)
+				return HttpResponseRedirect(reverse('lectut:coursepage', args=(course_id)))
+			else:
+				messages.error(request, 'You cannot post in this course')
+				return HttpResponseRedirect(reverse('lectut:coursepage', args=(course_id)))	
 
 
-
-def commenting(request,user_id, course_id, post_id):
+def commenting(request, course_id, post_id):
 		if request.method == 'POST':
-			user = get_object_or_404(User, pk=user_id)
+			user = request.user
+			if(hasattr(user, 'student')):
+				student = user.student
+				course = student.courses.all()
+			else:
+				proff = user.proff
+				course = proff.courses.all()
+					
+			course1 = get_object_or_404(Course, pk=course_id)
 			post = get_object_or_404(Post, pk=post_id)
-			post.comment_set.create(comment=request.POST['Comment'], commenter=user.username)
-			return HttpResponseRedirect(reverse('lectut:coursepage', args=(user.id,course_id)))	
+			if course1 in course:
+				post.comment_set.create(comment=request.POST['Comment'], commenter=user.username)
+				return HttpResponseRedirect(reverse('lectut:coursepage', args=(course_id)))	
+			else:
+				messages.error(request, 'You cannot comment on this course')
+				return HttpResponseRedirect(reverse('lectut:coursepage', args=(course_id)))
 
+
+def profilecommenting(request, course_id, post_id):
+		if request.method == 'POST':
+			user = request.user
+			if(hasattr(user, 'student')):
+				student = user.student
+				course = student.courses.all()
+			else:
+				proff = user.proff
+				course = proff.courses.all()
+					
+			course1 = get_object_or_404(Course, pk=course_id)
+			post = get_object_or_404(Post, pk=post_id)
+			if course1 in course:
+				post.comment_set.create(comment=request.POST['Comment'], commenter=user.username)
+				return HttpResponseRedirect(reverse('lectut:profile'))	
+			else:
+				messages.error(request, 'You cannot comment on this course')
+				return HttpResponseRedirect(reverse('lectut:profile'))				
 
 
 def userprofile(request, username):
@@ -124,3 +164,58 @@ def userprofile(request, username):
 	else:
 		messages.error(request, 'You have to login 1st')
 		return HttpResponseRedirect(reverse('lectut:index'))
+
+
+def homepage(request):
+	return HttpResponseRedirect(reverse('lectut:profile'))
+
+
+def signup(request):
+	if request.method == 'POST':
+		form = SignupForm(request.POST)
+		if form.is_valid():
+			user = User.objects.create_user(username=form.cleaned_data['username'],password=form.cleaned_data['password1'],email=form.cleaned_data['email'])
+			login(request, user)
+			if (request.POST['typ']=='Student'):
+				return HttpResponseRedirect(reverse('lectut:studentSignup'))
+			else:
+				return HttpResponseRedirect(reverse('lectut:proffSignup'))	
+		else :
+			return render(request, 'lectut/signup.html', {'form': form})
+	else:
+		form = SignupForm()
+		return render(request, 'lectut/signup.html', {'form': form})
+
+
+
+def studentSignup(request):
+	if request.method == 'POST':
+		form = StudentSignup(request.POST)
+		if form.is_valid():
+			f = form.save(commit=False)
+			f.user = request.user
+			f.save()
+			form.save_m2m()
+			return HttpResponseRedirect(reverse('lectut:profile'))
+		else:
+			return render(request, 'lectut/studentsignup.html', {'form': form})
+			#return HttpResponseRedirect(reverse('lectut:studentSignup'))	
+	form = StudentSignup()		
+	return render(request, 'lectut/studentsignup.html', {'form': form})		
+
+
+def proffSignup(request):
+	if request.method == 'POST':
+		form = ProffSignup(request.POST)
+		if form.is_valid():
+			f = form.save(commit=False)
+			f.user = request.user
+			f.save()
+			form.save_m2m()
+			return HttpResponseRedirect(reverse('lectut:profile'))
+		else:
+			return render(request, 'lectut/proffsignup.html', {'form': form})
+			#return HttpResponseRedirect(reverse('lectut:proffSignup'))	
+	form = ProffSignup()
+	return render(request, 'lectut/proffsignup.html', {'form': form})		
+
